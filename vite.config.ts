3f39774545873172ea8f2ed9eb9406b7d3bb5af7
Rémi Parpaillon <rparpaillon@tenable.com>
2023-12-13 11:42:40 +0100

    Fix lib not usable without skipLibCheck
    Bug reproduce repo: https://github.com/rparpa/toolbox-bug
    
    The lib can't be used as a dependency without using `skipLibCheck`
    typescript option. This error occurs:
    
    ```
    ➜  toolbox-bug git:(main) npx tsc
    node_modules/@productive-codebases/toolbox/dist/libs/logger/index.d.ts:1:19 - error TS2307: Cannot find module 'debug' or its corresponding type declarations.
    
    1 import debug from 'debug';
                        ~~~~~~~
    
    Found 1 error in node_modules/@productive-codebases/toolbox/dist/libs/logger/index.d.ts:1
    ```
    
    This error is not, as the index.d.ts file generated import debug which
    is only a dev dependency of the repo.
    
    To handle this issue:
    - Add debug as peerDependency (debug export on logger);
    - Add @types/debug as peerDependency (debug type export on logger);
    - Resolve @productive-codebases/toolbox to `src` in vite build (bonus,
      not needed).

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@productive-codebases/toolbox': path.resolve(__dirname, './src')
     }
   },
   build: {
